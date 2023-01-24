a9291d958c181f05082a8f685a43f4ce3833b4c2
PrathamPatel01 <90601027+PrathamPatel01@users.noreply.github.com>
2023-01-24 22:52:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const btn= document.querySelector('button');
+
+btn.addEventListener('mouseover',function(){
+    console.log("Moused overed me!");
+    const height=Math.floor(Math.random()*window.innerHeight);
+    const width=Math.floor(Math.random()*window.innerWidth);
+
+    // movements
+    btn.style.left=`${width}px`;
+    btn.style.top=`${height}px`;
+
+});
+
+btn.addEventListener('click',function(){
+btn.innerText='You Got Me!';
+document.body.style.backgroundColor='green';
+});
